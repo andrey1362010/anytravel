8d72f19bb5e1d8e7ce6af651fee5aba750166ac9
fobiasmog <zbrejkin@yandex.ru>
2014-04-21 00:53:50 +0400

    [anytravel] Registration form + some changes in templates logic

--- anytravel/anytravel_site/urls.py
+++ anytravel/anytravel_site/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth   import *
 from anytravel_site import views
 
 urlpatterns = patterns('',
-    url( r'^$', views.index, name='index' )
-    #url( r'^login/$', 'django.contrib.auth.views.login', name='login' )
+    url( r'^$', views.index, name='index' ),
+
+    url( r'^registration/$', views.registration, name='registration' )
 )
--- anytravel/anytravel_site/views.py
+++ anytravel/anytravel_site/views.py
@@ -3,4 +3,7 @@ from django.http      import HttpResponse
 from django.shortcuts import render, redirect
 
 def index( request ):
-	return render( request, 'index.html' )
+    return render( request, 'index.html' )
+
+def registration( request ):
+    return render( request, 'registration/index.html' )
